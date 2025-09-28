c0513016b2bb3e6ac40b93d5956afbacd1c57e38
Allaa <alaa.ghanem2004@icloud.com>
2025-09-28 21:36:12 +0300

    finished sebha tap

--- android/app/src/main/kotlin/com/example/islami_c16/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/islami_c16/MainActivity.kt
@@ -2,4 +2,4 @@ package com.example.islami_c16
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
--- lib/ui/home/widgets/tabs/SebhaTab/SebhaTab.dart
+++ lib/ui/home/widgets/tabs/SebhaTab/SebhaTab.dart
@@ -1,10 +1,122 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import '../../../../../core/resources/AssetsManager.dart';
+import '../../../../../core/resources/ColorManager.dart';
 
-class SebhaTab extends StatelessWidget {
+class SebhaTab extends StatefulWidget {
   const SebhaTab({super.key});
 
   @override
+  State<SebhaTab> createState() => _SebhaTabState();
+}
+
+class _SebhaTabState extends State<SebhaTab> {
+  int count = 0;
+  int index = 0;
+  double angle = 0;
+
+  List<String> tsabeh = [
+    'الحمد لله',
+    'الله اكبر',
+    'سبحان الله'
+  ];
+
+  void _incrementCounter() {
+    setState(() {
+      count++;
+      angle += pi / 12;
+
+      if (count == 33) {
+        count = 0;
+        index++;
+        if (index >= tsabeh.length) {
+          index = 0;
+        }
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.orange,);
+    double width = MediaQuery.of(context).size.width;
+
+    return Stack(
+      children: [
+        Image.asset('assets/images/sebha.png', fit: BoxFit.fill),
+        Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xff202020),
+                Colors.transparent,
+              ],
+            ),
+          ),
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: Image.asset(
+                AssetsManager.islamiLogo,
+                width: width * 0.75,
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            const SizedBox(height: 16),
+            Align(
+              alignment: Alignment.center,
+              child: Text(
+                'سَبِّحِ اسْمَ رَبِّكَ الأعلى',
+                style: TextStyle(
+                  color: ColorManager.whiteColor,
+                  fontSize: 36,
+                  fontWeight: FontWeight.w900,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+            ),
+            Image.asset('assets/images/Mask group.png', width: 73, height: 86),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: GestureDetector(
+                onTap: _incrementCounter,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Transform.rotate(
+                      angle: angle,
+                      child: Image.asset('assets/images/SebhaBody 1.png'),
+                    ),
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          tsabeh[index],
+                          style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.w700,
+                            color: ColorManager.whiteColor,
+                          ),
+                        ),
+                        Text(
+                          '$count',
+                          style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.w700,
+                            color: ColorManager.whiteColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
